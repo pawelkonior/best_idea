eccb4a636f1dee2b99f44f77583f77813c6ecddc
Dominika <dominika_mazur99@wp.pl>
2022-11-19 13:39:40 +0100

    add test component from react native paper to test it

--- mobile/App.js
+++ mobile/App.js
@@ -1,14 +1,21 @@
 import "react-native-gesture-handler";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
+import { Button } from "react-native-paper";
 
 export default function App() {
     return (
         <PaperProvider>
             <View style={styles.container}>
-                <Text>Open up App.js to start working on your app!</Text>
                 <StatusBar style="auto" />
+                <Button
+                    icon="camera"
+                    mode="contained"
+                    onPress={() => console.log("Pressed")}
+                >
+                    Test React Native Paper by click me
+                </Button>
             </View>
         </PaperProvider>
     );
