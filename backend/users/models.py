1e07e4cd31466e4cafd9fde3ffd6cc2eefbfd595
Martyna Witkowska <mar.witkowska@gmail.com>
2022-11-19 16:25:22 +0100

    extended abstract user model with long, lat, shopping frequency columns

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,8 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class CustomUser(AbstractUser):
-    pass
+    longitude = models.FloatField()
+    latitude = models.FloatField()
+    shopping_frequency = models.PositiveSmallIntegerField()
