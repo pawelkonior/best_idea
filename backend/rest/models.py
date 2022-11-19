7b2f793bde935ad121c7e3a75c208dd7737fecc3
MWasile <maaciej.wasilewski@gmail.com>
2022-11-19 22:54:11 +0100

    Change positiveintiger field to bigintigerfield, and fix model field issue

--- backend/rest/models.py
+++ backend/rest/models.py
@@ -15,7 +15,7 @@ class Product(models.Model):
 
 
 class ProductDetail(models.Model):
-    id = models.PositiveIntegerField(primary_key=True, auto_created=False)
+    id = models.BigIntegerField(primary_key=True, auto_created=False)
     image = models.TextField(null=True)
     name = models.CharField(max_length=200)
 
