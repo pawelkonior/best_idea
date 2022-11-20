0056a18351871f5cbae37b1126efd930b8f698f4
Pawel Konior <pawel.konior@gmail.com>
2022-11-20 11:59:18 +0100

    fixed size

--- web/src/App.js
+++ web/src/App.js
@@ -9,14 +9,14 @@ import ScannerView from "./components/Scanner/ScannerView";
 
 function App() {
     return (
-        <>
+        <div style={{width: '400px', margin: 'auto', position: 'relative'}}>
             <Routes>
                 <Route path={"/"} element={<MainView />} />
                 <Route path={"/detail"} element={<DetailProduct />} />
                 <Route path={"/search"} element={<SearchView />} />
                 <Route path={"/scan"} element={<ScannerView/>}/>
             </Routes>
-        </>
+        </div>
     );
 }
 
