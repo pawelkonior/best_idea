d6b3f7d14f730bbfea1981d6b7150ddb8f782284
Pawel Konior <pawel.konior@gmail.com>
2022-11-20 06:23:11 +0100

    init barcode scanner

---
+++ web/src/components/Scanner/Scanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CameraHandler from "./CameraHandler";
+
+function Scanner(props) {
+    return (
+        <div>
+            <CameraHandler />
+        </div>
+    );
+}
+
+export default Scanner;
---
+++ web/src/components/Scanner/Video.skeleton.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Loader from 'react-feather/dist/icons/loader';
+
+const VideoSkeleton = ({ error }) => (
+    <div className="skeleton__video">
+        {error ?
+            ""
+            :
+            <div className="skeleton__video--loading">
+                <Loader />
+            </div>
+        }
+    </div>
+);
+
+export default VideoSkeleton;
