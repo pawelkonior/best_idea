4882e2fcc7c775b8d388cffe8876316c9ea4abf6
olivergav <oliwier.gawlik0@gmail.com>
2022-11-20 06:29:57 +0100

    add footer component with styles

--- web/src/components/Footer.jsx
+++ web/src/components/Footer.jsx
@@ -4,25 +4,27 @@ import home from '../assets/images/home.svg';
 import user from '../assets/images/user.svg';
 import shopbag from '../assets/images/shopbag.svg';
 
-function Footer(){
+function Footer() {
     return (
-        <div className='container'>
-            <div className="button__wrapper">
-                <button className='button'>
-                    <img src={home} alt="" className="button__icon"/>
-                </button>
+        <div className='container__wrapper'>
+            <div className='container'>
+                <div className="button__wrapper">
+                    <button className='button'>
+                        <img src={home} alt="" className="button__icon"/>
+                    </button>
 
-                <button className='button'>
-                    <img src={lens} alt="" className="button__icon"/>
-                </button>
+                    <button className='button'>
+                        <img src={lens} alt="" className="button__icon"/>
+                    </button>
 
-                <button className='button'>
-                    <img src={shopbag} alt="" className="button__icon"/>
-                </button>
+                    <button className='button'>
+                        <img src={shopbag} alt="" className="button__icon"/>
+                    </button>
 
-                <button className='button'>
-                    <img src={user} alt="" className="button__icon"/>
-                </button>
+                    <button className='button'>
+                        <img src={user} alt="" className="button__icon"/>
+                    </button>
+                </div>
             </div>
         </div>
     )
