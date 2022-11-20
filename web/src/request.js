44f5db0be5c01ee2608b49b51b0e504901f21516
Krzysztof Plonka <krzysztof.plonka64@gmail.com>
2022-11-20 08:42:57 +0100

    Fixed cors

--- web/src/request.js
+++ web/src/request.js
@@ -10,8 +10,6 @@ export async function handleDataFromAPI(
         method,
         headers: {
             "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjcxNTAwOTUyLCJpYXQiOjE2Njg5MDg5NTIsImp0aSI6IjJjM2Y2NDE2YzJlZTQ2YTRiNjQ1OTlhODY1YWVkYjgwIiwidXNlcl9pZCI6MX0.1pxGdWVV2B9xlOgY7u4YZbUBjSmucHYJqsKIhhyCp3E",
         },
     };
@@ -27,5 +25,7 @@ export async function handleDataFromAPI(
     }
     console.log(response)
 
-    return await response.json();
+    const data = await response.json()
+    console.log("Data", data)
+    return data
 }
